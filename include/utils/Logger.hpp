ac0d491d38aa8b0b16646df1b30c8ec559a1321b
MAIDic <qpalzm0147@gmial.com>
2025-09-15 11:12:59 +0800

    將日誌改成Json line 格式

--- include/utils/Logger.hpp
+++ include/utils/Logger.hpp
@@ -8,17 +8,22 @@
 #include <string>
 #include <iostream>
 
-std::shared_ptr<spdlog::logger> create_logger(const std::string& name, const std::string& filePath,const std::string& pattern){
+std::shared_ptr<spdlog::logger> create_logger(const std::string& name, const std::string& filePath, 
+        const std::string& pattern = "[%Y-%m-%d %H:%M:%S][%t][%^%l%$] %v", 
+        const std::string& json_pattern = {"{\"date\": \"%Y-%m-%d\", \"time\": \"%H:%M:%S\", \"level\": \"%^%l%$\", \"process\": %P, \"thread\": %t, \"message\": \"%v\"}"} ){
      try {
                 std::vector<spdlog::sink_ptr> sinks;
-                sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-                sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>(filePath, 1048576 * 5, 3));
+
+                auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+                console_sink->set_pattern(pattern);
+                sinks.push_back(console_sink);
+
+                auto rotating_file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(filePath, 1048576 * 5, 3);
+                rotating_file_sink->set_pattern(json_pattern);
+                sinks.push_back(rotating_file_sink);
 
                 // 建立一個名為 "server" 的 logger
                 auto logger = std::make_shared<spdlog::async_logger>(name, begin(sinks), end(sinks), spdlog::thread_pool(), spdlog::async_overflow_policy::block);
-
-                // 設定 logger 的格式
-                logger->set_pattern(pattern);
 
                 // 註冊 logger 
                 spdlog::register_logger(logger);
